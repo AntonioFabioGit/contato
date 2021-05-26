25ed60ec1246f8fd6140b5e904fd6311fe05559b
fabioheller <fabioheller@gmail.com>
2021-05-26 18:24:42 -0300

    Atualizado descrição do endpoint no swagger

--- src/main/java/br/com/contato/controller/ContatoController.java
+++ src/main/java/br/com/contato/controller/ContatoController.java
@@ -2,6 +2,7 @@ package br.com.contato.controller;
 
 import br.com.contato.entity.Contato;
 import br.com.contato.service.ContatoService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,32 +18,33 @@ public class ContatoController {
     @Autowired
     private ContatoService contatoService;
 
-    //Inserir novo contato
-    @PostMapping(name = "Inserir novo contato", consumes = "application/json")
+    @ApiOperation(value = "Inserir novo contato")
+    @PostMapping( consumes = "application/json")
     public ResponseEntity<?> newContato ( @RequestBody Contato contato){
         return new ResponseEntity<Contato>(contatoService.newContato(contato), HttpStatus.CREATED);
     }
 
-    @PutMapping(path = ("/atualizar_por_id"), name = "Atualizar contato por ID", consumes = "application/json")
+    @ApiOperation(value = "Atualizar contato por ID")
+    @PutMapping(path = ("/atualizar_por_id"), consumes = "application/json")
     public ResponseEntity<?> updateContatoById ( @RequestBody Contato contato, @RequestParam("id") @NotBlank Long id){
         return ResponseEntity.ok().body(contatoService.updateContatoById(contato, id));
     }
 
-    //Deletar contato por Id
+    @ApiOperation(value = "Deletar contato por ID")
     @DeleteMapping(path = "/deletar_por_id", name = "Deletar contato por ID")
     public ResponseEntity<?> deleteContatoById ( @RequestParam("id") @NotBlank Long id){
         contatoService.deleteContatoById(id);
         return ResponseEntity.ok().build();
     }
 
-    //Listar contato por id
-    @GetMapping(path = ("/buscar_por_id"), name = "Buscar contato por ID")
+    @ApiOperation(value = "Buscar contato por ID")
+    @GetMapping(path = ("/buscar_por_id"))
     public ResponseEntity<Contato> searchContatoById ( @RequestParam("id") @NotBlank Long id){
         return ResponseEntity.ok().body(contatoService.searchContatoById(id));
     }
 
-    //Listar todos contatos
-    @GetMapping(name = "Listar contatos")
+    @ApiOperation(value = "Listar todos contatos")
+    @GetMapping
     public ResponseEntity<List<Contato>> listContatos(){
         return ResponseEntity.ok().body(contatoService.listContato());
     }
