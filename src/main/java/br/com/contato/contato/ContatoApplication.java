e1ca5d633f74d7a5e429ec86402be5ca67ffba54
fabioheller <fabioheller@gmail.com>
2021-05-26 12:11:37 -0300

    inserido projeto no repositório

---
+++ src/main/java/br/com/contato/contato/ContatoApplication.java
@@ -0,0 +1,13 @@
+package br.com.contato.contato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContatoApplication.class, args);
+	}
+
+}
